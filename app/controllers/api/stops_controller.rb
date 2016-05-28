ae7db99920e662da523c443dd5041643809cdd10
Matias Korhonen <matias@kiskolabs.com>
2016-05-28 12:57:47 +0300

    Adds some HTTP cache headers

--- app/controllers/api/stops_controller.rb
+++ app/controllers/api/stops_controller.rb
@@ -2,11 +2,15 @@ class Api::StopsController < Api::BaseController
   skip_before_action :require_icloud_user_id
 
   def index
+    expires_in 2.minutes, public: true
+
     stops = Stop.all
     render json: StopRepresenter.for_collection.new(stops)
   end
 
   def show
+    expires_in 2.minutes, public: true
+
     stop = Stop.find(params[:id])
     render json: StopRepresenter.new(stop)
   end

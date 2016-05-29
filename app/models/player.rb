0588b203b32cddd31f54ccbe6a796839b0acd12c
Matias Korhonen <matias@kiskolabs.com>
2016-05-29 12:53:52 +0300

    Whoops, forgot to commit

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,5 +1,6 @@
 class Player < ApplicationRecord
   has_many :attempts, dependent: :destroy
+  has_many :attempt_stops, via: :attempts
 
   validates :icloud_user_id, presence: true, uniqueness: true
 

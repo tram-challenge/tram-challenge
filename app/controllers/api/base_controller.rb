de34697be0dba9ab56e349206a83d9e59ff39b68
Matias Korhonen <matias@kiskolabs.com>
2016-05-30 21:00:59 +0300

    Adds specs for the leaderboard

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -2,7 +2,12 @@ class Api::BaseController < ApplicationController
   protect_from_forgery with: :null_session
   skip_before_action :verify_authenticity_token
 
+  before_action :set_default_response_format
   before_action :require_icloud_user_id
+
+  private def set_default_response_format
+    request.format = :json
+  end
 
   private def current_player
     @current_player ||= Player.find_or_create_by(icloud_user_id: params[:icloud_user_id])
--- spec/controllers/api/leaderboards_controller_spec.rb
+++ spec/controllers/api/leaderboards_controller_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Api::LeaderboardsController, type: :controller do
+  render_views
 
+  let(:attempts) {
+    3.times.map do |i|
+      player = Player.find_or_create_by(
+        nickname: "Player ##{i}",
+        icloud_user_id: SecureRandom.uuid
+      )
+      player.attempts.create(started_at: ((i+1)*5).hours.ago, finished_at: (i+1).hours.ago)
+    end
+  }
+
+  describe "GET show" do
+
+    it "renders an empty list of attempts" do
+      attempts
+
+      get :show
+      expect(response.status).to eq(200)
+
+      attempts = JSON.parse(response.body)
+      expect(attempts.map {|a| a.fetch("position") }).to eq([1, 2, 3])
+
+      expect(attempts.map {|a| a.fetch("name") }).to eq(["Player #0", "Player #1", "Player #2"])
+    end
+
+  end
 end

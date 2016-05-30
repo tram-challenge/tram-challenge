71d46f677cf722f1000708f9e14fa45a370d8b40
Matias Korhonen <matias@kiskolabs.com>
2016-05-30 19:02:01 +0300

    Some stupid specs for the pages

--- Gemfile
+++ Gemfile
@@ -65,6 +65,7 @@ group :development do
 end
 
 group :test do
+  gem "rails-controller-testing"
   gem "shoulda-matchers", "~> 3.1"
   gem "rspec_junit_formatter", "~> 0.2.3"
   gem "simplecov", require: false
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,9 +3,34 @@ require 'rails_helper'
 RSpec.describe PagesController, type: :controller do
 
   describe "GET #home" do
-    it "returns http success" do
+    it "renders the home page" do
       get :home
       expect(response).to have_http_status(:success)
+      expect(response).to render_template(:home)
+    end
+  end
+
+  describe "GET #map" do
+    it "renders the map page" do
+      get :map
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template(:map)
+    end
+  end
+
+  describe "GET #about" do
+    it "renders the about page" do
+      get :about
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template(:about)
+    end
+  end
+
+  describe "GET #rules" do
+    it "renders the rules page" do
+      get :rules
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template(:rules)
     end
   end
 

d412fcc703df14bed4f108c54a089f17689f8e74
Matias Korhonen <matias@kiskolabs.com>
2016-06-11 15:43:06 +0300

    Allow the stops to be downloaded as KML too

--- app/controllers/stops_controller.rb
+++ app/controllers/stops_controller.rb
@@ -2,5 +2,11 @@ class StopsController < ApplicationController
   def index
     set_tab :stops
     @stops = Stop.active.order(:name)
+
+    respond_to do |format|
+      format.html
+      format.kml
+      format.json
+    end
   end
 end

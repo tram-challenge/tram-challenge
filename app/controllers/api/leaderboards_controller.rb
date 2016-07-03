d7df852116745f3cf73ce39ec8568426571a6e89
Stephen Sykes <sdsykes@gmail.com>
2016-07-03 17:41:35 +0300

    Only send valid results over leaderboard api

--- app/controllers/api/leaderboards_controller.rb
+++ app/controllers/api/leaderboards_controller.rb
@@ -2,6 +2,6 @@ class Api::LeaderboardsController < Api::BaseController
   skip_before_action :require_icloud_user_id
 
   def show
-    @attempts = Attempt.fully_completed.order_by_elapsed
+    @attempts = Attempt.fully_completed_and_valid
   end
 end
